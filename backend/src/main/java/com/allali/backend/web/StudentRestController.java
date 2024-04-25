5a3bf79d7f1480677300694966af8947a8f9e7c9
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-04-25 18:24:48 +0100

    Added functionality to generate PDF for payment processing

--- backend/src/main/java/com/allali/backend/web/StudentRestController.java
+++ backend/src/main/java/com/allali/backend/web/StudentRestController.java
@@ -2,14 +2,22 @@ package com.allali.backend.web;
 
 import com.allali.backend.entities.Payment;
 import com.allali.backend.entities.Student;
+import com.allali.backend.enums.PaymentStatus;
+import com.allali.backend.enums.PaymentType;
 import com.allali.backend.repositories.PaymentRepository;
 import com.allali.backend.repositories.StudentRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
@@ -37,6 +45,29 @@ public class StudentRestController {
     @GetMapping("payments/{code}")
     public List<Payment> allPaymentsByCode(@PathVariable String code){
         return paymentRepository.findByStudentCode(code);
+    }
+
+    @PostMapping(value = "/payments",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public  Payment savePayment(@RequestParam MultipartFile file ,
+                                LocalDate date , double amount , PaymentType type,
+                                String studentCode) throws IOException {
+        Path path = Paths.get(System.getProperty("user.home"),"students-app","payments");
+        if(!Files.exists(path)){
+            Files.createDirectories(path);
+        }
+        String fileId = UUID.randomUUID().toString();
+        Path filePath = Paths.get(System.getProperty("user.home"),"students-app","payments",fileId+".pdf");
+        Files.copy(file.getInputStream(),filePath);
+        Student student = studentRepository.findByCode(studentCode);
+        Payment payment = Payment.builder()
+                .type(type)
+                .amount(amount)
+                .date(date)
+                .status(PaymentStatus.CREATED)
+                .student(student)
+                .file(filePath.toUri().toString()).build();
+        Payment savedPayment = paymentRepository.save(payment);
+        return savedPayment;
     }
 
 }
